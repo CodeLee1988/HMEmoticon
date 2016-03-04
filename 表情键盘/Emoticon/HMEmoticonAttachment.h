1a33adc5663b3911153811be2a9e272f12f1ef95
刀哥 <liufan321@gmail.com>
2016-03-05 02:58:52 +0800

    添加 HMEmoticonAttachment 抽取属性文本创建方法／记录附件文字

---
+++ 表情键盘/Emoticon/HMEmoticonAttachment.h
@@ -0,0 +1,13 @@
+//
+//  HMEmoticonAttachment.h
+//  表情键盘
+//
+//  Created by 刘凡 on 16/3/5.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMEmoticonAttachment : NSTextAttachment
+
+@end
---
+++ 表情键盘/Emoticon/HMEmoticonAttachment.m
@@ -0,0 +1,13 @@
+//
+//  HMEmoticonAttachment.m
+//  表情键盘
+//
+//  Created by 刘凡 on 16/3/5.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import "HMEmoticonAttachment.h"
+
+@implementation HMEmoticonAttachment
+
+@end
