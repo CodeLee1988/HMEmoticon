ff4343ed82f6b5e920f78e97f08015cc7f3552cd
刀哥 <liufan321@gmail.com>
2016-03-05 03:53:52 +0800

    添加最近使用表情数组

--- 表情键盘/Emoticon/HMEmoticonManager.m
+++ 表情键盘/Emoticon/HMEmoticonManager.m
@@ -12,7 +12,9 @@
 // 每页显示的表情数量
 static NSInteger kEmoticonsCountOfPage = 20;
 
-@implementation HMEmoticonManager
+@implementation HMEmoticonManager {
+    NSMutableArray <HMEmoticon *> *_recentEmoticonList;
+}
 
 #pragma mark - 单例 & 构造函数
 + (instancetype)sharedManager {
@@ -61,24 +63,29 @@ static NSInteger kEmoticonsCountOfPage = 20;
 
 #pragma mark - 最近使用表情
 - (void)addRecentEmoticon:(HMEmoticon *)emoticon {
-    NSLog(@"%s", __FUNCTION__);
-    
     // 0. 表情计数 ++
     emoticon.times++;
     
     // 1. 判断表情是否已经存在
-    if (![_packages[0].emoticonsList containsObject:emoticon]) {
-        [_packages[0].emoticonsList addObject:emoticon];
+    if (![_recentEmoticonList containsObject:emoticon]) {
+        [_recentEmoticonList addObject:emoticon];
     }
     
     // 2. 排序
-    [_packages[0].emoticonsList sortUsingComparator:^NSComparisonResult(HMEmoticon *obj1, HMEmoticon *obj2) {
+    [_recentEmoticonList sortUsingComparator:^NSComparisonResult(HMEmoticon *obj1, HMEmoticon *obj2) {
         return obj1.times < obj2.times;
     }];
+    
+    // 3. 设置第[0]组表情包数组
+    NSInteger length = _recentEmoticonList.count < kEmoticonsCountOfPage ? _recentEmoticonList.count : kEmoticonsCountOfPage;
+    _packages[0].emoticonsList = [_recentEmoticonList subarrayWithRange:NSMakeRange(0, length)].mutableCopy;
 }
 
 #pragma mark - 加载表情包数据
 - (void)loadPackages {
+    // 0. 加载最近使用表情列表
+    _recentEmoticonList = [NSMutableArray array];
+    
     // 1. 读取 emoticons.plist
     NSString *path = [[NSBundle hm_emoticonBundle] pathForResource:@"emoticons.plist" ofType:nil];
     NSArray *array = [NSArray arrayWithContentsOfFile:path];
